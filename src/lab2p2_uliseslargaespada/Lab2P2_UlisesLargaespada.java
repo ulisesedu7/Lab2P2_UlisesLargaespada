2ca757749e19b23a676232b1a4273045f94d58cf
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-01-27 13:41:27 -0600

    Create new clases for the lab

--- src/lab2p2_uliseslargaespada/Lab2P2_UlisesLargaespada.java
+++ src/lab2p2_uliseslargaespada/Lab2P2_UlisesLargaespada.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab2p2_uliseslargaespada;
+
+import java.util.*;
 
 /**
  *
